60caff562e63f488f9e8511c0b29583e092cc31b
Elke Dick <elke.codes@gmail.com>
2021-12-03 03:25:40 -0800

    move state from hero to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@
 // -- IMPORT SCSS -- //
 import "./App.scss";
 
+// -- IMPORT MODULES --//
+import React, { Component } from "react";
+
+// -- IMPORT DATA
+import jsonData from "./data/videos.json";
+import jsonDataDetail from "./data/video-details.json";
+
 // -- IMPORT COMPONENTS -- //
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
@@ -10,17 +17,34 @@ import Comments from "./components/Comments/Comments"
 import VideosNext from "./components/VideosNext/VideosNext";
 import Button from "./components/Button/Button";
 
+
+
+
 // -- COMPONENT DECLARATION -- //
-function App() {
-  return (
-    <>
+
+
+class App extends Component {
+
+  state = {
+    videosDetail : jsonDataDetail,
+    selectedVideo : jsonDataDetail[0]
+  }
+
+
+  render() {
+    return (
+      <>
       <Header />
-      <Hero />
+      <Hero videosDetail={this.state.videosDetail} selectedVideo={this.state.selectedVideo}/>
       <Comments />
       <VideosNext />
       <Button />
     </>
-  );
+    );
+  }
 }
+
+
+
 
 export default App;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,7 @@
 /// --- HERO.JS --- ///
 // -- IMPORT MODULES -- //
 import React, { Component } from 'react';
-// -- IMPORT DATA
-import jsonData from "../../data/videos.json";
-import jsonDataDetail from "../../data/video-details.json";
+
 // -- IMPORT COMPONENTS -- //
 // import Date from "../Date/Date";
 import VideoDetails from './VideoDetails/VideoDetails';
@@ -13,30 +11,25 @@ import "./Hero.scss";
 
 
 // -- COMPONENT DECLARATION -- //
-class Hero extends Component {
 
-  state = {
-    videos : jsonDataDetail,
-    selectedVideo : jsonDataDetail[0]
-  }
 
-  render() {
+const Hero = (props) => {
+  console.log(props);
+  const { image, title} = props.selectedVideo;
+  return (
 
-    return (
-    
-        <section className="Hero">
-            {/* <Date /> */}
-            <video className="Hero__video" src="" poster={this.state.selectedVideo.image}></video>
+    <section className="Hero">
+        {/* <Date /> */}
+        <video className="Hero__video" src="" poster={image}></video>
 
-            <article className="Hero__video-details">
-              <h2 className="Hero__title">{this.state.selectedVideo.title}</h2>
-              <DeviderLine />
-             <VideoDetails selectedVideo={this.state.selectedVideo}/>
-            </article>
-        </section>
-      
-    );
-  }
-}
+        <article className="Hero__video-details">
+          <h2 className="Hero__title">{title}</h2>
+          <DeviderLine />
+         <VideoDetails selectedVideo={props.selectedVideo} videosDetails={props.videosDetails}/>
+        </article>
+    </section>
+  
+);
+};
 
 export default Hero;
