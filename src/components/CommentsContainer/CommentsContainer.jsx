0bdfd9b6fe12e4ec5175b246cc2ce8ebd12ee58d
Elke Dick <elke.codes@gmail.com>
2021-12-05 22:23:46 -0800

    key is comment timestamp

--- src/components/CommentsContainer/CommentsContainer.jsx
+++ src/components/CommentsContainer/CommentsContainer.jsx
@@ -2,10 +2,6 @@
 
 import React from 'react';
 import Comment from '../Comment/Comment';
-// import Avatar from "../Avatar/Avatar";
-// import { v4 as uuid } from "uuid";
-
-
 
 // -- IMPORT SCSS -- //
 import "./CommentsContainer.scss";
