625bca26119ed0e8f1276f33d61ecf35ec6485f4
Elke Dick <elke.codes@gmail.com>
2021-12-15 20:01:25 -0800

    add enable post video on server side

--- server/index.js
+++ server/index.js
@@ -7,18 +7,27 @@ const { v4: uuid } = require("uuid");
 // A helper function to read a data JSON file
 const readData = () => {
 	// The readFileSync path is relative to where server.js file is
-	const videosData = fs.readFileSync("./data/video.json");
+	const videosData = fs.readFileSync("./data/videos.json");
 	return JSON.parse(videosData);
 };
+
+// A helper function that writes the data to JSON file
+const writeFile = (videosData) => {
+	// JSON.stringify takes additional parameters, that allow us to specify the amounts of white space (ie, indentation) in the file
+	fs.writeFileSync("./data/videos.json", JSON.stringify(videosData, null, 2));
+};
+
+//be able to send json when post
+app.use(express.json());
 
 // Fetching a list of games: GET /games
 app.get("/videos", (req, res) => {
 	const videosData = readData();
 
-	newVideosData = videosData.map((videoData) => {
+	newVideosData = videosData.map((videoDataShort) => {
 		// 	// Here we are using a rest parameter to remove some of the keys from the game object, which is common for GET all requests, to return limited data
 		// videosDataShort = videosData.map((video) => {
-		const {
+		let {
 			description,
 			views,
 			likes,
@@ -27,7 +36,7 @@ app.get("/videos", (req, res) => {
 			timestamp,
 			comments,
 			...videoInfo
-		} = videoData;
+		} = videoDataShort;
 		return videoInfo;
 	});
 
@@ -39,7 +48,7 @@ app.get("/videos", (req, res) => {
 
 app.get("/videos/:videoID", (req, res) => {
 	const videosData = readData();
-	console.log(req.params);
+	// console.log(req.params);
 	const video = videosData.find((video) => video.id === req.params.videoID);
 
 	if (!video) {
@@ -52,4 +61,46 @@ app.get("/videos/:videoID", (req, res) => {
 // Listen on PORT and provide a success callback function
 app.listen(8080, () => {
 	console.log("🚀 Server listening on 8080");
+});
+
+app.post("/videos", (req, res) => {
+	const videosData = readData();
+	console.log(req);
+	// It's a good idea to setup validation for your endpoints to make sure the data required is sent in a request
+	if (
+		!req.body.title ||
+		// !req.body.channel ||
+		// !req.body.image ||
+
+		!req.body.description
+	) {
+		return res
+			.status(400)
+			.send(
+				"Please make sure to include title,  and description of the video"
+			);
+	}
+
+	// We create IDs on a server, so it's not going to be a part of a request, rather we can use uuid or similar library to generate the new id
+	const newVideo = {
+		//TODO grab title and description from upload form
+		title: req.body.title,
+		channel: "TODO something with this",
+		image: "TODO hardcoded?",
+		description: req.body.description,
+		views: "TODO grab the views from the client",
+		likes: "TODO grab the likes from the client",
+		video: "TODO check what to do, actually make able to upload a video? api call to mock video?",
+		timestamp: new Date(),
+		comments: ["TODO grab comments from client"],
+		id: uuid()
+		// image: hardcoded?
+	};
+
+	// // Update our gamesData array and then write the updates to a games data JSON file
+	videosData.push(newVideo);
+	writeFile(videosData);
+
+	// // For POST requests we will send back a 201(Created) status code and it's common to send the newly created object (in case we need that id) in a response
+	res.status(201).json(newVideo);
 });
